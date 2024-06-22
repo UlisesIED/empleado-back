50068eddc3612291f3296da7e967a5d44207c689
UlisesIED <uliescodi@gmail.com>
2024-06-22 15:54:18 -0600

    modificacion superUser

--- users/admin.py
+++ users/admin.py
@@ -3,17 +3,17 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from users.models import Users
 
 class UserAdmin(BaseUserAdmin):
-    list_display = ('username', 'is_staff', 'is_active', 'is_superuser')
+    list_display = ('username', 'is_staff', 'is_active', 'is_superuser', 'role')
     search_fields = ('username',)
     ordering = ('username',)
     fieldsets = (
-        (None, {'fields': ('username', 'password')}),
+        (None, {'fields': ('username', 'password', 'role')}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
     )
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser'),
+            'fields': ('username','role', 'password1', 'password2', 'is_staff', 'is_active', 'is_superuser'),
         }),
     )
 
--- users/models.py
+++ users/models.py
@@ -16,19 +16,19 @@ class Persona(models.Model):
         
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, password=None, **extra_fields):
+    def create_user(self, username, role= None, password=None, **extra_fields):
         if not username:
             raise ValueError('The Username field must be set')
-        user = self.model(username=username, **extra_fields)
+        user = self.model(username=username,  role=role, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, password=None, **extra_fields):
+    def create_superuser(self, username, role = None, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        return self.create_user(username, password, **extra_fields)
+        return self.create_user(username, role, password, **extra_fields)
 
 class Users(AbstractBaseUser, PermissionsMixin):
     
@@ -51,4 +51,4 @@ class Users(AbstractBaseUser, PermissionsMixin):
     
     objects = UserManager()
     USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = []    
+    REQUIRED_FIELDS = ['role']    
